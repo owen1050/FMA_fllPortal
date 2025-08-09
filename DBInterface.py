c3122ff4a8abaa3338885f385e089a010e34b51a
Owen Busler <owen1050@gmail.com>
2025-08-08 21:31:32 -0400

    api and db work

--- DBInterface.py
+++ DBInterface.py
@@ -1,1 +1,11 @@
 import sqlite3
+
+class dbInterface:
+	con = sqlite3.connect("testing.db", check_same_thread=False)
+	cur = con.cursor()
+
+	def testConnection(self):
+		res = self.cur.execute("SELECT * FROM teams")
+		return "1"
+
+db = dbInterface()
--- server.py
+++ server.py
@@ -3,16 +3,23 @@ from flask import request
 from flask import send_file
 from threading import Thread
 
+from DBInterface import dbInterface
+
 
 app = Flask(__name__, static_folder='static', static_url_path='')
+db = dbInterface()
 
 @app.route('/')
 def main():
 	return app.send_static_file('main.html')
     
-@app.route('/api/isup')
-def apiIsUp():
+@app.route('/api/isServerUp')
+def apiIsServerUp():
 	return "1"
+
+@app.route('/api/isDBUp')
+def apiIsDBUp():
+	return db.testConnection()
 
 
 if __name__ == '__main__':
